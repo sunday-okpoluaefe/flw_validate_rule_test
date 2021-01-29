9b77c358f3e930a3882414368f0dcf54a3c1d4be
Sunday Okpoluaefe <sunday@bookingsafrica.com>
2021-01-29 09:56:18 +0100

    refactored validator

--- src/api/controllers/rule.js
+++ src/api/controllers/rule.js
@@ -12,7 +12,7 @@ controller.get = async ( req, res, next) => {
             "name": "Sunday Okpoluaefe",
             "github": "@sunday-okpoluaefe",
             "email": "sunday.okpoluaefe@gmail.com",
-            "mobile": "08051525444",
+            "mobile": "08051524444",
             "twitter": ""
         }
     })
